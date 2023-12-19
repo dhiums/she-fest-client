a8220af54e26738fad43dd1dfa38d96cd0139b2f
MUHAMMED HISHAM <cdg.prjs@gmail.com>
2023-12-20 00:19:06 +0530

    Update user authentication logic to use bcrypt hashing algorithm

--- src/components/programs/Programs.tsx
+++ src/components/programs/Programs.tsx
@@ -128,56 +128,75 @@ function Programs(props: Props) {
 
               {(data.roles == Roles.Controller ||
                 data.roles == Roles.Admin) && (
-                  <div className="flex flex-col gap-2">
-                    <p className="text-primary text-xl font-semibold text-center">
-                      Select Team
-                    </p>
-                    <select
-                      className="w-72 px-4 py-2 rounded-xl border-2 border-dashed border-brown"
-                      onChange={(e) => {
-                        setTeam(e.target.value);
-                      }}
-                    >
-                      <option value="">Select Team</option>
-                      {props.team?.map((team, index) => (
-                        <option key={index} value={team?.name as string}>
-                          {team.name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                )}
+                <div className="flex flex-col gap-2">
+                  <p className="text-primary text-xl font-semibold text-center">
+                    Select Team
+                  </p>
+                  <select
+                    className="w-72 px-4 py-2 rounded-xl border-2 border-dashed border-brown"
+                    onChange={(e) => {
+                      setTeam(e.target.value);
+                    }}
+                  >
+                    <option value="">Select Team</option>
+                    {props.team?.map((team, index) => (
+                      <option key={index} value={team?.name as string}>
+                        {team.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              )}
 
-              {
-                data.roles == Roles.TeamManager && <div
+              {data.roles == Roles.TeamManager && (
+                <div
                   className="flex items-center justify-center gap-1 text-center font-semibold text-lg bg-brown text-white px-2 py-1 rounded-lg cursor-pointer"
                   onClick={() => {
-                    router.push(`/admin/programs/download?team=${data.team?.name}`);
+                    router.push(
+                      `/admin/programs/download?team=${data.team?.name}`
+                    );
                   }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-.53 14.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V8.25a.75.75 0 00-1.5 0v5.69l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z" clipRule="evenodd" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-.53 14.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V8.25a.75.75 0 00-1.5 0v5.69l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z"
+                      clipRule="evenodd"
+                    />
                   </svg>
 
                   <p> Candidate's List</p>
                 </div>
-              }
+              )}
 
-
-              {
-                data.roles != Roles.TeamManager && <div
+              {data.roles != Roles.TeamManager && (
+                <div
                   className="flex items-center justify-center gap-1 text-center font-semibold text-lg bg-brown text-white px-2 py-1 rounded-lg cursor-pointer"
                   onClick={() => {
                     router.push(`/admin/programs/download?team=${team}`);
                   }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-.53 14.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V8.25a.75.75 0 00-1.5 0v5.69l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z" clipRule="evenodd" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-.53 14.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V8.25a.75.75 0 00-1.5 0v5.69l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z"
+                      clipRule="evenodd"
+                    />
                   </svg>
 
                   <p> Candidate's List</p>
                 </div>
-              }
+              )}
             </div>
           </div>
         </div>
@@ -187,7 +206,6 @@ function Programs(props: Props) {
             <p className="text-center font-extrabold text-3xl text-brown mb-2">
               Program Search
             </p>
-
           </div>
           <div className="md:w-2/3 flex gap-2">
             <input
@@ -251,73 +269,121 @@ function Programs(props: Props) {
                     {data.roles == Roles.TeamManager
                       ? program.type == Types.Single
                         ? program.candidateProgramme?.filter((cp) => {
-                          return cp.candidate?.team?.name == data.team?.name;
-                        }).length +
-                        "/" +
-                        program.candidateCount
+                            return cp.candidate?.team?.name == data.team?.name;
+                          }).length +
+                          "/" +
+                          program.candidateCount
                         : program.candidateProgramme?.filter((cp) => {
-                          return cp.candidate?.team?.name == data.team?.name;
-                        }).length +
-                        "/" +
-                        program.groupCount
+                            return cp.candidate?.team?.name == data.team?.name;
+                          }).length +
+                          "/" +
+                          program.groupCount
                       : program.type == Types.Single
-                        ? program.candidateProgramme?.length +
+                      ? program.candidateProgramme?.length +
                         "/" +
                         (program.candidateCount as number) * 60
-                        : program.candidateProgramme?.length +
+                      : program.candidateProgramme?.length +
                         "/" +
                         (program.groupCount as number) * 60}
                   </p>
                 </div>
                 {(data.roles == Roles.Controller ||
                   data.roles == Roles.Admin) && (
-                    <div className="flex w-full justify-between">
-                      <button
-                        onClick={() => {
-                          setIsUpdate(true);
-                          setSelected(program);
-                        }}
-                        className="bg-white border border-dashed border-primary rounded-xl px-4 py-2 "
+                  <div className="flex w-full justify-between">
+                    {/* <button
+                      onClick={() => {
+                        setIsUpdate(true);
+                        setSelected(program);
+                      }}
+                      className="bg-white border border-dashed border-primary rounded-xl px-4 py-2 "
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-brown"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 text-brown"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                          />
-                        </svg>
-                      </button>
-                      <button
-                        onClick={() => {
-                          setIsDelete(true);
-                          setSelected(program);
-                        }}
-                        className="bg-white border border-dashed border-primary rounded-xl px-4 py-2 "
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </button>
+                    <button
+                      onClick={() => {
+                        setIsDelete(true);
+                        setSelected(program);
+                      }}
+                      className="bg-white border border-dashed border-primary rounded-xl px-4 py-2 "
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-red-600"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 text-red-600"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  )}
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button> */}
+                    <button className="bg-white border border-dashed border-primary rounded-xl px-4 py-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-brown"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                        />
+                      </svg>
+                    </button>
+                    <button className="bg-white border border-dashed border-primary rounded-xl px-4 py-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-brown"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                        />
+                      </svg>
+                    </button>
+                    <button className="bg-white border border-dashed border-primary rounded-xl px-4 py-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-brown"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                )}
               </div>
             ))}
           </div>
