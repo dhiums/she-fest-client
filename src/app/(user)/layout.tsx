71906e0d8b4f1d2e340b87256f524ce92fddf00c
MUHAMMED HISHAM <cdg.prjs@gmail.com>
2023-12-21 02:21:59 +0530

    Add Footer component and update imports in layout.tsx and result/page.tsx

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,9 +1,9 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Headers/NavHeader";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <Header />
       {children}
     </div>
   );
--- src/app/(user)/result/page.tsx
+++ src/app/(user)/result/page.tsx
@@ -1,7 +1,42 @@
-export default function Page(){
-    return(
-        <div>
-            <p>Hi</p>
-        </div>
-    )
+import Result from "@/components/result/Result";
+import {
+  GetAllProgrammesDocument,
+  GetAllProgrammesQuery,
+  GetAllProgrammesQueryVariables,
+  GetAllZonesDocument,
+  GetAllZonesQuery,
+  GetAllZonesQueryVariables,
+  FindResultEnteredProgrammesByZoneDocument,
+  FindResultEnteredProgrammesByZoneQuery,
+  FindResultEnteredProgrammesByZoneQueryVariables,
+  Programme,
+  Zone,
+} from "@/gql/graphql";
+import { API_KEY } from "@/lib/env";
+import { getUrqlClient } from "@/lib/urql";
+
+export default async function page({ params, searchParams }: any) {
+  const { client } = getUrqlClient();
+
+  const zones = await client.query<GetAllZonesQuery, GetAllZonesQueryVariables>(
+    GetAllZonesDocument,
+    {}
+  );
+
+  const enteredProgrammes = await client.query<
+    FindResultEnteredProgrammesByZoneQuery,
+    FindResultEnteredProgrammesByZoneQueryVariables
+  >(FindResultEnteredProgrammesByZoneDocument, {
+    zone: searchParams.zone,
+  });
+
+  return (
+    <Result
+      zones={zones.data?.zones as Zone[]}
+      enteredPrograms={
+        enteredProgrammes.data?.findResultEnteredProgrammesByZone as Programme[]
+      }
+      zone={searchParams.zone}
+    />
+  );
 }
