881e2e956ad147dde2225ff29f1db34f40bccae2
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-12-23 15:37:23 +0530

    Refactor user page layout and add animations

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -12,7 +12,11 @@ export default function Page() {
     <>
       <section className="min-h-screen justify-between flex flex-col">
         <div className="flex justify-between bg-h-line bg-contain h-40 items-center px-8">
-          <img src="/img/logo/logo.svg" alt="she fest logo" className="h-[120px]" />
+          <img
+            src="/img/logo/logo.svg"
+            alt="she fest logo"
+            className="h-[120px]"
+          />
           <img
             src="/img/logo/flowers/orange.svg"
             alt="orange flower"
@@ -28,11 +32,20 @@ export default function Page() {
           </nav>
         </div>
         <p className="px-20 text-[6vw] leading-[6vw] font-bold text-center">
-          Brushing <span className="bg-yellow  font-mono">Colours</span>, <br /> <span className="text-brown font-serif animate-pulse">Bridging</span> Cultures
+          Brushing <span className="bg-yellow  font-mono">Colours</span>, <br />{" "}
+          <span className="text-brown font-serif animate-pulse">Bridging</span>{" "}
+          Cultures
         </p>
         <div className="flex justify-between bg-h-line bg-contain h-40 items-end">
           <button className="bg-yellow ml-12 mb-10 flex items-center gap-1 animate-bounce text-lg px-3 py-2 border-black shadow-md border-2 rounded-md font-semibold">
-          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="16"
+              width="16"
+              viewBox="0 0 512 512"
+            >
+              <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+            </svg>
             explore
           </button>
           <img
@@ -42,15 +55,15 @@ export default function Page() {
           />
         </div>
       </section>
-      <section className="h-screen relative">
+      <section className="h-screen relative flex flex-col justify-between">
+        <div className="bg-h-line bg-contain h-40"></div>
+        <img
+          src="/img/logo/flowers/yellow.svg"
+          alt="yellow flower"
+          className="h-80 animate-spin-slow absolute -bottom-32 -left-32"
+        />
 
-      <img
-            src="/img/logo/flowers/yellow.svg"
-            alt="yellow flower"
-            className="h-80 animate-spin-slow absolute -bottom-32 -left-32"
-          />
-
-
+        <div className="bg-h-line bg-contain h-40"></div>
       </section>
     </>
   );
