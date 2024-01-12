eae2b7c2b9c9a87c1520603c4530d7ff16c94d09
MuhammedHashimT <hashimt4567@gmail.com>
2024-01-12 17:14:39 +0530

    Update CreateInput to CreateIInput

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -188,7 +188,7 @@ export type CreateGradeInput = {
   pointSingle: Scalars['Int']['input'];
 };
 
-export type CreateInput = {
+export type CreateIInput = {
   inputs: Array<CreateCandidateInput>;
 };
 
